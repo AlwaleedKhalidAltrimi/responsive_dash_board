8e2c790852cedf620c3d77eff780609abd92cc1b
AlwaleedKhalidAltrimi <alwaleedaltrimi@gmail.com>
2025-08-31 18:45:19 +0300

    first commit

---
+++ lib/widgets/all_expenses_items_list_view.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/models/all_expenses_item_model.dart';
+import 'package:responsive_dash_board/utils/app_images.dart';
+import 'all_expenses_item.dart';
+
+class AllExpensesItemsListView extends StatefulWidget {
+  const AllExpensesItemsListView({super.key});
+
+  @override
+  State<AllExpensesItemsListView> createState() =>
+      _AllExpensesItemsListViewState();
+}
+
+class _AllExpensesItemsListViewState extends State<AllExpensesItemsListView> {
+  final items = [
+    const AllExpensesItemModel(
+        image: Assets.imagesBalance,
+        title: 'Balance',
+        date: 'April 2022',
+        price: r'$20,129'),
+    const AllExpensesItemModel(
+        image: Assets.imagesIncome,
+        title: 'Income',
+        date: 'April 2022',
+        price: r'$20,129'),
+    const AllExpensesItemModel(
+        image: Assets.imagesExpenses,
+        title: 'Expenses',
+        date: 'April 2022',
+        price: r'$20,129'),
+  ];
+
+  int selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: GestureDetector(
+            onTap: () {
+              updateIndex(0);
+            },
+            child: AllExpensesItem(
+              isSelected: selectedIndex == 0,
+              itemModel: items[0],
+            ),
+          ),
+        ),
+        const SizedBox(
+          width: 8,
+        ),
+        Expanded(
+          child: GestureDetector(
+            onTap: () {
+              updateIndex(1);
+            },
+            child: AllExpensesItem(
+              isSelected: selectedIndex == 1,
+              itemModel: items[1],
+            ),
+          ),
+        ),
+        const SizedBox(
+          width: 8,
+        ),
+        Expanded(
+          child: GestureDetector(
+            onTap: () {
+              updateIndex(2);
+            },
+            child: AllExpensesItem(
+              isSelected: selectedIndex == 2,
+              itemModel: items[2],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  void updateIndex(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+}
---
+++ lib/widgets/transaction_history_list_view.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import '../models/transaction_model.dart';
+import 'transaction_item.dart';
+
+class TransactionHistoryHeaderHistoryListView extends StatelessWidget {
+  const TransactionHistoryHeaderHistoryListView({super.key});
+
+  static const items = [
+    TransactionModel(
+        title: 'Cash Withdrawal',
+        date: '13 Apr, 2022 ',
+        amount: r'$20,129',
+        isWithdrawal: true),
+    TransactionModel(
+        title: 'Landing Page project',
+        date: '13 Apr, 2022 ',
+        amount: r'$20,129',
+        isWithdrawal: false),
+    TransactionModel(
+        title: 'Juni Mobile App project',
+        date: '13 Apr, 2022 ',
+        amount: r'$20,129',
+        isWithdrawal: false),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: items.map((e) => TransactionItem(transactionModel: e)).toList(),
+    );
+  }
+}
